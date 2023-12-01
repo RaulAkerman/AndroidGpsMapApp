fca40a68cb92ff78b52d23c203973ae9e9a32bfc
raaker <raaker@ttu.ee>
2023-12-01 16:28:00 +0200

    Commit 4 added saving checkpoints when rotating

--- app/src/main/java/com/example/androidgpsmapapp/PolyLineViewModel.kt
+++ app/src/main/java/com/example/androidgpsmapapp/PolyLineViewModel.kt
@@ -2,7 +2,9 @@ package com.example.androidgpsmapapp
 
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 
 class PolylineViewModel : ViewModel() {
     var polylinePoints: MutableList<LatLng>? = null
+    var checkpoints: MutableList<Marker>? = null
 }
