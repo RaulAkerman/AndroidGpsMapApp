7038b58ff35a036b9d2e73e651ff2fc7c10449ab
kkinks <kkinks@ttu.ee>
2024-01-10 12:28:29 +0200

    Added UI elements for registering account, login and loading data.

--- app/src/main/java/com/example/androidgpsmapapp/SettingsActivity.kt
+++ app/src/main/java/com/example/androidgpsmapapp/SettingsActivity.kt
@@ -4,14 +4,18 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var buttonSettingsApply: Button
     private lateinit var editTextNumberMinimumSpeed: EditText
     private lateinit var editTextNumberMaximumSpeed: EditText
+    private lateinit var buttonRegister: Button
+    private lateinit var buttonLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -19,10 +23,20 @@ class SettingsActivity : AppCompatActivity() {
         editTextNumberMinimumSpeed = findViewById(R.id.editTextNumberMinimumSpeed)
         editTextNumberMaximumSpeed = findViewById(R.id.editTextNumberMaximumSpeed)
         buttonSettingsApply = findViewById(R.id.buttonSettingsApply)
+        buttonRegister = findViewById(R.id.buttonRegister)
+        buttonLogin = findViewById(R.id.buttonLogin)
 
         buttonSettingsApply.setOnClickListener {
             saveSpeedValues()
             finish()
+        }
+
+        buttonRegister.setOnClickListener {
+            showRegisterDialog()
+        }
+
+        buttonLogin.setOnClickListener {
+            showLoginDialog()
         }
 
         val prefs = getSharedPreferences("Prefs", Context.MODE_PRIVATE)
@@ -44,5 +58,67 @@ class SettingsActivity : AppCompatActivity() {
         editor.putInt("maxSpeed", maxSpeed)
 
         editor.apply()
+    }
+
+    private fun showRegisterDialog() {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.register_dialog, null)
+        val emailEditText: EditText = dialogView.findViewById(R.id.editTextEmail)
+        val firstNameEditText: EditText = dialogView.findViewById(R.id.editTextFirstName)
+        val lastNameEditText: EditText = dialogView.findViewById(R.id.editTextLastName)
+        val passwordEditText: EditText = dialogView.findViewById(R.id.editTextPassword)
+        val confirmPasswordEditText: EditText = dialogView.findViewById(R.id.editTextConfirmPassword)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setView(dialogView)
+            .setTitle("Register")
+            .setPositiveButton("Register") { dialog, _ ->
+                val email = emailEditText.text.toString()
+                val firstName = firstNameEditText.text.toString()
+                val lastName = lastNameEditText.text.toString()
+                val password = passwordEditText.text.toString()
+                val confirmPassword = confirmPasswordEditText.text.toString()
+
+                // Perform registration. Temp to show input values
+                println("Email: $email")
+                println("First Name: $firstName")
+                println("Last Name: $lastName")
+                println("Password: $password")
+                println("Confirm Password: $confirmPassword")
+
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    private fun showLoginDialog() {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.login_dialog, null)
+        val emailEditText: EditText = dialogView.findViewById(R.id.editTextLoginEmail)
+        val passwordEditText: EditText = dialogView.findViewById(R.id.editTextLoginPassword)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setView(dialogView)
+            .setTitle("Register")
+            .setPositiveButton("Register") { dialog, _ ->
+                // Handle registration logic here
+                val email = emailEditText.text.toString()
+                val password = passwordEditText.text.toString()
+
+                // Perform Login. Temp to show input values
+                println("Email: $email")
+                println("Password: $password")
+
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        val alertDialog = builder.create()
+        alertDialog.show()
     }
 }
